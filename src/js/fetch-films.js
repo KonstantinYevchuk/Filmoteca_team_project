cb1e0651cd971fdf2ebf5062b629e37b558d9247
Titotrapatonni <titovtelexfree@gmail.com>
2023-01-25 00:50:22 +0200

    add functions getFetch

---
+++ src/js/fetch-films.js
@@ -0,0 +1,43 @@
+// ====================== README ============================
+// for import // import { getPopularMoviesFetch, getSearchMoviesFetch } from './js/fetch-films';
+// getPopularMoviesFetch(page) //
+// getSearchMoviesFetch(query, page) //
+// page = current page // Number
+// query = query from input // String
+// ====================== THE END ===========================
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '158819e65eb0fbf8513ba7b934c25216';
+
+async function getPopularMoviesFetch(page) {
+  try {
+    const response = await fetch(
+      `${API_URL}/trending/movie/day?api_key=${API_KEY}&page=${page || 1}`
+    );
+    const popularMovies = await response.json();
+    console.log(popularMovies);
+    return popularMovies;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function getSearchMoviesFetch(query, page) {
+  if (!query) {
+    return;
+  }
+  try {
+    const response = await fetch(
+      `${API_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${
+        page || 1
+      }`
+    );
+    const searchMovies = await response.json();
+    console.log(searchMovies);
+    return searchMovies;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export { getPopularMoviesFetch, getSearchMoviesFetch };
