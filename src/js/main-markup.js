5768b05375673776733b806eee110836c7d35586
Titotrapatonni <titovtelexfree@gmail.com>
2023-01-26 13:28:56 +0200

    delete test functions

--- src/js/main-markup.js
+++ src/js/main-markup.js
@@ -3,12 +3,12 @@ import { createCardMarkup } from './markup-watch-q';
 
 const galleryEl = document.querySelector('.js-gallery');
 
-export function createPopularMoviesMarkup(page) {
-  getPopularMoviesFetch(page)
-    .then(data => {
-      const movies = data.results;
-      const markup = movies.map(movie => createCardMarkup(movie));
-      galleryEl.innerHTML = markup;
-    })
-    .catch(err => console.log(err));
-}
+// export function createPopularMoviesMarkup(page) {
+//   getPopularMoviesFetch(page)
+//     .then(data => {
+//       const movies = data.results;
+//       const markup = movies.map(movie => createCardMarkup(movie));
+//       galleryEl.innerHTML = markup;
+//     })
+//     .catch(err => console.log(err));
+// }
