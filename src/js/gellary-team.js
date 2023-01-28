bce428443c5411c21d08df79bb49abab0682b694
RuslanTenderyts <rylya1995@gmail.com>
2023-01-28 18:12:07 +0200

    Update gellary-team.js
    має бути з крапкою

--- src/js/gellary-team.js
+++ src/js/gellary-team.js
@@ -2,92 +2,157 @@ export const galleryTeam = [
     {
         id:  1,
         name:  "Orest Mykhalchuk",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Orest_Mykhalchuk_150.jpg",
         photo: "/images/team/Orest_Mykhalchuk_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Orest_Mykhalchuk_150.jpg",
+        photo: "./images/team/Orest_Mykhalchuk_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "Scrum Master",
     },
     {
         id:  2,
         name:  'Konstantin Yevchuk',
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Konstantin_Yevchuk_150.jpg",
         photo: "/images/team/Konstantin_Yevchuk_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Konstantin_Yevchuk_150.jpg",
+        photo: "./images/team/Konstantin_Yevchuk_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "Team Lead",
     },
     {
         id:  3,
         name:  'Eugene Titov',
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Yevgen_Titov_150.jpg",
         photo: "/images/team/Yevgen_Titov_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Yevgen_Titov_150.jpg",
+        photo: "./images/team/Yevgen_Titov_450.jpg" ,
+>>>>>>> Stashed changes
         position: "developer" ,
     },
     {
         id:  4,
         name:  'Olexander Yanchenko',
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Olexander_Yanchenko_150.jpg",
         photo: "/images/team/Olexander_Yanchenko_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Olexander_Yanchenko_150.jpg",
+        photo: "./images/team/Olexander_Yanchenko_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "developer",
     },
     {
         id:  5,
         name:  "Sergiy Maxymchuk",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Sergiy_Maxymchuk_150.jpg",
         photo: "/images/team/Sergiy_Maxymchuk_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Sergiy_Maxymchuk_150.jpg",
+        photo: "./images/team/Sergiy_Maxymchuk_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "developer",
     },
     {
         id:  6,
         name:  "Alexey Ryzhenko",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Aleksey_Ryzhenko_150.jpg",
         photo: "/images/team/Aleksey_Ryzhenko_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Aleksey_Ryzhenko_150.jpg",
+        photo: "./images/team/Aleksey_Ryzhenko_450.jpg" ,
+>>>>>>> Stashed changes
         position: "developer" ,
     },
     {
         id:  7,
         name:  "Ruslan Tenderyts",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Ruslan_Tenderyts_150.jpg",
         photo: "/images/team/Ruslan_Tenderyts_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Ruslan_Tenderyts_150.jpg",
+        photo: "./images/team/Ruslan_Tenderyts_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "developer",
     },
     {
         id:  8,
         name:  "Vladyslav Rohalov",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Vladyslav_Rohalov_150.jpg",
         photo: "/images/team/Vladyslav_Rohalov_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Vladyslav_Rohalov_150.jpg",
+        photo: "./images/team/Vladyslav_Rohalov_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "developer",
     },
     {
         id:  9,
         name:  "Svitlana Kozachenko",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Svitlana_Kozachenko_150.jpg",
         photo: "/images/team/Svitlana_Kozachenko_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Svitlana_Kozachenko_150.jpg",
+        photo: "./images/team/Svitlana_Kozachenko_450.jpg" ,
+>>>>>>> Stashed changes
         position: "developer" ,
     },
     {
         id:  10,
         name:  "Volodymyr Zankevych",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Volodymyr_Zankevych_150.jpg",
         photo: "/images/team/Volodymyr_Zankevych_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Volodymyr_Zankevych_150.jpg",
+        photo: "./images/team/Volodymyr_Zankevych_450.jpg" ,
+>>>>>>> Stashed changes
         position: "developer" ,
     },
     {
         id:  11,
         name:  "Olexandr Kutsenko",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Olexandr_Kutsenko_150.jpg",
         photo: "/images/team/Olexandr_Kutsenko_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Olexandr_Kutsenko_150.jpg",
+        photo: "./images/team/Olexandr_Kutsenko_450.jpg" ,
+>>>>>>> Stashed changes
         position: "developer" ,
     },
     {
         id:  12,
         name:  "Viacheslav Borysov",
+<<<<<<< Updated upstream
         photoPreview:  "/images/team/Viacheslav_Borysov_150.jpg",
         photo: "/images/team/Viacheslav_Borysov_450.jpg" ,
+=======
+        photoPreview:  "./images/team/Viacheslav_Borysov_150.jpg",
+        photo: "./images/team/Viacheslav_Borysov_450.jpg" ,
+>>>>>>> Stashed changes
         position:  "developer",
     },
     // {
     //     id:  13,
     //     name:  "Yuriy Grechyha",
+<<<<<<< Updated upstream
     //     photoPreview:  "/images/team/",
     //     photo: "/images/team/" ,
+=======
+    //     photoPreview:  "./images/team/",
+    //     photo: "./images/team/" ,
+>>>>>>> Stashed changes
     //     position:  "developer",
     // },
 ]
