a27ab04d2cf71ed41f03a8ee6631dec08a075be6
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-01-30 13:55:22 +0200

    fix

--- src/js/watch-local-storadge.js
+++ src/js/watch-local-storadge.js
@@ -1,9 +1,9 @@
 import { Notify } from 'notiflix';
 import { createCardMarkup } from './main-markup';
 
-import { getCards } from './modal';
-import pagination from './pagination';
-import { getUpcomingMovies } from './fetch-films';
+// import { getCards } from './modal';
+// import pagination from './pagination';
+// import { getUpcomingMovies } from './fetch-films';
 
 
 const galleryEl = document.querySelector('.js-gallery');
