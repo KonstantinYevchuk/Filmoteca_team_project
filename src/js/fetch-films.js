8b6cbf1b9a56289b289cd50e70b0d23d722c10be
Vladyslav Rohalov <domus.lg@gmail.com>
2023-01-30 13:18:51 +0100

    Add interactive to loader (spinner)
    1. Modify loader.js Now loader started when page uploading and stop when page loading complete.
    2. Add loader to fetch-films.js
    3. Add loader to fetch-trailer.js
    4. Mod loader html (loader active at start)
    5. Fixing bugs in loader.scss

--- src/js/fetch-films.js
+++ src/js/fetch-films.js
@@ -5,12 +5,14 @@
 // page = current page // Number
 // query = query from input // String
 // ====================== THE END ===========================
+import onPageLoadingSpinner from './loader';
 
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '158819e65eb0fbf8513ba7b934c25216';
 
 async function getPopularMoviesFetch(page) {
   try {
+    onPageLoadingSpinner();
     const response = await fetch(
       `${API_URL}/trending/movie/day?api_key=${API_KEY}&page=${page || 1}`
     );
@@ -31,6 +33,7 @@ async function getSearchMoviesFetch(query, page) {
     return;
   }
   try {
+    onPageLoadingSpinner();
     const response = await fetch(
       `${API_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${
         page || 1
@@ -50,6 +53,7 @@ async function getSearchMoviesFetch(query, page) {
 
 async function getUpcomingMovies(page) {
   try {
+    onPageLoadingSpinner();
     const response = await fetch(
       `${API_URL}/movie/upcoming?api_key=${API_KEY}&page=${page || 1}`
     );
--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,24 @@
-// import { addLoader, removeLoader } from './js/loader';
-// to start loader -> run function addLoader();
-// to stop loader -> run function removeLoader();
+// import onPageLoadingSpinner from './js/loader';
+// onPageLoadingSpinner();
+// need to import to index.js and library.js
 
 const refs = { loader: document.querySelector('.loader') };
 
-export function removeLoader() {
+function removeLoader() {
+  refs.loader.classList.add('loader-hidden');
+}
+
+function addLoader() {
   refs.loader.classList.remove('loader-hidden');
 }
 
-export function addLoader() {
-  refs.loader.classList.add('loader-hidden');
+document.addEventListener('readystatechange', onPageLoadingSpinner);
+
+export default function onPageLoadingSpinner() {
+  const imgRef = document.querySelector('.movie__poster');
+
+  addLoader();
+  if (document.readyState === 'complete') {
+    setTimeout(removeLoader, 500);
+  }
 }
