b22209128b1d67b8e7d066e37ea15cec2a484fe8
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-01-31 20:29:12 +0200

    Update fetch-trailer.js

--- src/js/fetch-trailer.js
+++ src/js/fetch-trailer.js
@@ -1,10 +1,8 @@
-const API_URL = 'https://api.themoviedb.org/3/movie/';
-const API_KEY = '158819e65eb0fbf8513ba7b934c25216';
-import onPageLoadingSpinner from './loader';
+import { refs } from "./refs";
 
 export async function fetchTrailer(id = 536554) {
   try {
-    return await fetch(`${API_URL}${id}/videos?api_key=${API_KEY}`).then(
+    return await fetch(`${refs.API_URL}movie/${id}/videos?api_key=${refs.API_KEY}`).then(
       resp => {
         if (!resp.ok) {
           throw new Error('Bad request!!!');
